f58b7f1dfcb8a5031b909ed71a76a1b0c5900fb0
Foksid <slavyan149@gmail.com>
2025-06-18 20:30:11 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+BOT_TOKEN = "ВАШ_ТОКЕН"  # Получи у @BotFather
+ADMIN_ID = 123456789       # Твой Telegram ID
+TELEGRAM_CHANNEL_USERNAME = "@foksid322"
+DISCUSSION_CHAT_USERNAME = "@foksid322test"
+
+# Настройки БД (заглушка, можно заменить на реальную реализацию)
+DATABASE_URL = None
