33ba5f40ff4bce3fecc0251fcbea947a35afae14
Foksid <slavyan149@gmail.com>
2025-06-18 20:53:29 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 # config.py
+import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 YOUTUBE_CHANNEL_ID = "UCGS02-NLVxwYHwqUx7IFr3g"
-ADMIN_ID = "@kvd322"
+ADMIN_ID: int = int(os.getenv("ADMIN_ID", "1003278206"))  # Числовой ID админа
 TELEGRAM_CHANNEL_USERNAME = "@foksid322"
 DISCUSSION_CHAT_USERNAME = "@foksid322test"
 
